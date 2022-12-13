6c6bb63c1e2fddf42563a22805e82d07d59e5ce7
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-12-13 19:49:56 +0530

    SQL File With Stored ProceduresSQL

---
+++ EMPD1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace EMPD1
+{
+    public class EMPD1
+    {
+        public static string str = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public void connect_database()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Create Database Ado_Payroll_Service", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            EMPD1 obj=new EMPD1();
+            obj.connect_database();
+        }
+    }
+}
